2897c8318ed1e8a480fc1946bbfdeaf0dbd3e98c
LKurdi292 <kurdilora@gmail.com>
2021-06-26 22:22:14 +0200

    initial commit

---
+++ models/Sauce.js
@@ -0,0 +1,21 @@
+// Importation de mongoose
+const mongoose = require('mongoose');
+
+
+// Creation d'un schéma Sauce
+const sauceSchema = mongoose.Schema({
+    userId: {type: String, required: true}, 
+    name: {type: String, required: true}, 
+	manufacturer: { type: String, required: true},
+	description: {type: String, required: true},
+	mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required:true},
+	heat: {type: Number, required: true},
+	likes: {type: Number, required: true},
+	dislikes: {type: Number, required: true},
+	usersLiked: {type: [String], required: true},
+	usersDisliked: {type: [String], required: true},
+});
+
+// Exporter le modele
+module.exports = mongoose.model('Sauce', sauceSchema);
