b788b24bb41336f3ba885e1e1174fc9980c8837a
Lora Kurdi <kurdilora@gmail.com>
2021-07-16 17:34:23 +0200

    modifySauceCheck middleware functional

--- backend/middleware/modifySauceCheck.js
+++ backend/middleware/modifySauceCheck.js
@@ -2,14 +2,11 @@ const fs = require('fs');
 
 module.exports = (req, res, next) => {
 	try {
-		// vaut undefined quand il n'y pas de nouvelle image
-		console.log(req.body);
-
-		// normal que ça vale undefined quand il n'y pas de nouvelle image
-		console.log(req.file);
-
 		// nouvelle image
 		if (req.file) {
+			console.log(req.body.sauce);
+			console.log(req.file);
+
 			const sauce = JSON.parse(req.body.sauce);
 	
 			// enlever les espaces en début et fin de string
@@ -22,7 +19,7 @@ module.exports = (req, res, next) => {
 				// si tout est ok, on peut passer la requête au prochain middleware
 				// mais on vérifie avant s'il y a une nouvelle image, sinon on supprime l'ancienne image sauvegardée en doublon par multer
 				next();
-				console.log("les champs sont BONS, et il a une nouvelle image");
+				console.log("les champs sont BONS, et il y a une nouvelle image");
 
 			} else {
 				console.log("les champs sont FAUX, et il y a une nouvelle image");
@@ -31,10 +28,22 @@ module.exports = (req, res, next) => {
 				})
 			}
 		} else {
-			console.log("Il n'y a PAS de nouvelle image");
-			// vaut undefined ???
-			console.log(req.body.sauce);
-			next();
+			// il n'y pas de nouvelle image
+			console.log(req.body);
+
+			// enlever les espaces en début et fin de string
+			const name = req.body.name.trim();
+			const manufacturer = req.body.manufacturer.trim();
+			const description = req.body.description.trim();
+			const mainPepper = req.body.mainPepper.trim();
+
+			if (name.length > 0 && manufacturer.length > 0 && description.length > 0 && mainPepper.length > 0) {
+				console.log("Les champs sont BONS, et il n'y a PAS de nouvelle image");
+				next();
+			} else {
+				console.log("Les champs sont FAUX, et il n'y a PAS de nouvelle image");
+				res.status(400).json({ error: 'Requête non valable' });
+			}
 		} 
 	} catch (error) {
 		console.log(error);
